cc3043942976693dbae94d05048f5baf8c5b530e
ghada <ghada.ajimi@etu.univ-cotedazur.fr>
2022-11-30 15:39:51 +0100

    save

--- serveur/app/index.js
+++ serveur/app/index.js
@@ -1,8 +1,9 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
 const clients = new Map();
 
-console.log("Server connected on port 8080");
+const wss = new WebSocket.Server({ port: 8080 },()=>{
+    console.log("Server started");
+});
 
 wss.on('connection', (ws) => {
     const id = uuidv4();
@@ -10,21 +11,27 @@ wss.on('connection', (ws) => {
     const metadata = {id, color};
 
     clients.set(ws, metadata);
+    console.log("New client connected\n");
 
     ws.on('message', (messageAsString) => {
         console.log("message received : \n");
-        console.log(messageAsString);
+        console.log(messageAsString.toString());
+        ws.send(messageAsString.toString());
     });
 
     ws.on("close", () => {
         clients.delete(ws);
         console.log("Client removed");
     });
-})
+});
+
+wss.on('listening',()=>{
+    console.log('server listening on 8080')
+});
 
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
-}
+};
