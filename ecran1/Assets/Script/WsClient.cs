97fae9416cd2141d8defa87b11554ed271ef91a2
EvaRadu <eva.radu@etu.univ-cotedazur.fr>
2022-11-30 15:26:17 +0100

    adding the client script

--- ecran1/Assets/Script/WsClient.cs
+++ ecran1/Assets/Script/WsClient.cs
@@ -5,18 +5,45 @@ public class WsClient : MonoBehaviour
     WebSocket ws;
     private void Start()
     {
-        ws = new WebSocket("ws://ea06-134-59-215-253.eu.ngrok.io/ws");
+        
+        ws = new WebSocket("ws://ece3-134-59-215-253.eu.ngrok.io/ws");
        
-        ws.Connect();
         Debug.Log("Connected");
         ws.OnMessage += (sender, e) =>
         {
             Debug.Log("Message Received from "+((WebSocket)sender).Url+", Data : "+e.Data);
         };
+
+        ws.OnError += (sender, e) =>
+        {
+            Debug.Log("error : " + e);
+        };
+
+        ws.OnOpen += (sender, e) =>
+        {
+            Debug.Log("open : " + e);
+        };
+
+        ws.Connect();
+
+        /*
+
+        ws = new WebSocket("ws://ece3-134-59-215-253.eu.ngrok.io/ws");
+
+        using (var ws = new WebSocket ("ws://dragonsnest.far/Laputa")) {
+        ws.OnMessage += (sender, e) =>
+            Debug.Log("Message Received from "+((WebSocket)sender).Url+", Data : "+e.Data);
+        ws.Connect ();
+        ws.Send ("HELLO");
+        //Console.ReadKey (true);
+      }
+      */
     }
 private void Update()
     {
-        if(ws == null)
+        try
+        {
+            if(ws == null)
         {
             return;
         }
@@ -24,6 +51,12 @@ if (Input.GetKeyDown(KeyCode.Space))
         {
             ws.Send("Hello");
             Debug.Log("Message Sent");
-        }  
+        }
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+          
     }
 }
