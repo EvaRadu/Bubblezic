2a09e70ff5c21e21703d1deb6b7241954e75c928
EvaRadu <evaradu@hotmail.fr>
2022-12-09 21:20:31 +0100

    working on #11 : disparition of the bubbles after some seconds

--- ecran1/Assets/Script/Bubble.cs
+++ ecran1/Assets/Script/Bubble.cs
@@ -6,26 +6,53 @@ public class Bubble : MonoBehaviour
 {
     [SerializeField] private Color color;
     [SerializeField] private SpriteRenderer _srenderer;
+    float duration;
+
+    public void Start()
+    {
+        duration = 5;
+    }
+
 
     public void setColor(Color color)
     {
         this.color = color;
         _srenderer.material.color = color;
+
     }
 
+
+
     public void Update(){
+
+        duration -= Time.deltaTime;   
+        gameObject.SetActive(true);
+
+        if (duration <= 0)
+        {
+            if (gameObject.activeSelf)   {  gameObject.SetActive(false);}            
         
-        if(Input.GetMouseButtonDown(0)){ // from le R
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(mousePos);
-        RaycastHit2D hitinfo = Physics2D.Raycast(new Vector2(mousePos.x,mousePos.y), Vector2.zero);       
-        if (hitinfo.collider != null){
-            Debug.Log("Clicked on the bubble"); 
-            Destroy(hitinfo.collider.gameObject);
-            }
         }
-        }
+            else { gameObject.SetActive(true);}
         
+
+            
+
+            if(Input.GetMouseButtonDown(0)){ // from le R
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Debug.Log(mousePos);
+                RaycastHit2D hitinfo = Physics2D.Raycast(new Vector2(mousePos.x,mousePos.y), Vector2.zero);       
+                if (hitinfo.collider != null){
+                    Debug.Log("Clicked on the bubble"); 
+                    Destroy(hitinfo.collider.gameObject);
+                    }
+                }
     }
+}
+        
+        
+       
+        
+    
 
    
