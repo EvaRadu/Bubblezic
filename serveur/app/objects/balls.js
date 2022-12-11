092c7c7512935ad963358250ad8d04f08f242a80
kmay-z <khaoula.bouhlal@etu.univ-cotedazur.fr>
2022-12-11 14:56:24 +0100

    generation de balle en back_end, test  #8

--- serveur/app/objects/balls.js
+++ serveur/app/objects/balls.js
@@ -22,4 +22,23 @@ module.exports = listBalles = [{
     rayon: 4,
     temps: 0.2,
     type: 1
-}];
+},{
+    id:1,
+    posX:-12,
+    posY:20,
+    couleur: '19459A' ,
+    rayon: 4,
+    temps: 0.4,
+    type: 0
+},{
+    id:1,
+    posX:30,
+    posY:-18,
+    couleur: '18959A' ,
+    rayon: 4,
+    temps: 0.6,
+    type: 0
+}
+
+
+];
