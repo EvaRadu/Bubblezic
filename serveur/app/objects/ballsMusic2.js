9a55f6a42a29476e844efa5df2fd54f438db768b
EvaRadu <evaradu@hotmail.fr>
2023-01-22 18:27:40 +0100

    working on #74 : 2nd music

--- serveur/app/objects/ballsMusic2.js
+++ serveur/app/objects/ballsMusic2.js
@@ -1,12 +1,12 @@
 module.exports = listBalles = [
 
-    // BULLE SIMPLE 
+    // BULLES SIMPLES 
    
     { 
         id:0,
         typeName : "bubble",
-        posX: -1,
-        posY: 1,
+        posX: -4,
+        posY: 0,
         couleur: 'magenta',
         rayon: 2,
         temps: 0.5,
@@ -16,6 +16,183 @@ module.exports = listBalles = [
         side: 0,
         texture: "bubble2"
     },
+
+    { 
+        id:-1,
+        typeName : "bubble",
+        posX: -4,
+        posY: -3,
+        couleur: 'red',
+        rayon: 2,
+        temps: 1,
+        type: 0,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "bubble2"
+    },
+
+    { 
+        id:2,
+        typeName : "bubble",
+        posX: -4,
+        posY: 3,
+        couleur: 'yellow',
+        rayon: 2,
+        temps: 1.5,
+        type: 0,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "bubble2"
+    },
+    { 
+        id:3,
+        typeName : "bubble",
+        posX: -4,
+        posY: 0,
+        couleur: 'blue',
+        rayon: 2,
+        temps: 2,
+        type: 0,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "bubble2"
+    },
+    { 
+        id:4,
+        typeName : "bubble",
+        posX: -4,
+        posY: -3,
+        couleur: 'green',
+        rayon: 2,
+        temps: 2.5,
+        type: 0,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "bubble2"
+    },
+
+    { 
+        id:5,
+        typeName : "bubble",
+        posX: 4,
+        posY: 0,
+        couleur: 'magenta',
+        rayon: 2,
+        temps: 0.5,
+        type: 0,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "bubble2"
+    },
+
+    { 
+        id:6,
+        typeName : "bubble",
+        posX: 4,
+        posY: -3,
+        couleur: 'red',
+        rayon: 2,
+        temps: 1,
+        type: 0,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "bubble2"
+    },
+
+    { 
+        id:7,
+        typeName : "bubble",
+        posX: 4,
+        posY: 3,
+        couleur: 'yellow',
+        rayon: 2,
+        temps: 1.5,
+        type: 0,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "bubble2"
+    },
+    { 
+        id:8,
+        typeName : "bubble",
+        posX: 4,
+        posY: 0,
+        couleur: 'blue',
+        rayon: 2,
+        temps: 2,
+        type: 0,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "bubble2"
+    },
+    { 
+        id:9,
+        typeName : "bubble",
+        posX: 4,
+        posY: -3,
+        couleur: 'green',
+        rayon: 2,
+        temps: 2.5,
+        type: 0,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "bubble2"
+    },
+
+    // PUZZLE
+    {
+        id: 10,
+        typeName: "bubble",
+        posX: -2.5,
+        posY: 0,
+        couleur: 'white',
+        rayon: 2,
+        temps: 3,
+        type: 7,
+        duration: 1.5,
+        rotation: 90,
+        side: 1,
+        texture: ""
+    },
+    {
+        id: 11,
+        typeName: "bubble",
+        posX: 2.5,
+        posY: 0,
+        couleur: 'white',
+        rayon: 2,
+        temps: 3,
+        type: 7,
+        duration: 1.5,
+        rotation: -90,
+        side: 2,
+        texture: ""
+    },
+    {
+        id: 12,
+        typeName: "bubble",
+        posX: 0,
+        posY: 0,
+        couleur: 'white',
+        rayon: 2,
+        temps: 3,
+        type: 6,
+        duration: 1.5,
+        rotation: 90,
+        side: 0,
+        texture: "ciblePuzzle"
+    },
+
+    
 
 
    
